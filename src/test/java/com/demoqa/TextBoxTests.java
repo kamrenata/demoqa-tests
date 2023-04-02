d1356478911877653023109767fd73ffa2d6ec95
kamrenata <renatakamal@gmail.com>
2023-04-02 18:37:18 +0400

    added TextBoxtests

---
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -0,0 +1,29 @@
+package com.demoqa;
+
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests extends TestBase {
+
+
+    @Test
+    void successfulFillFormTest(){
+       open("https://demoqa.com/text-box");
+
+       $("#userName").setValue("John");
+       $("#userEmail").setValue("test@email.com");
+       $("#currentAddress").setValue("Nowhere");
+       $("#permanentAddress").setValue("Here");
+       $(byText("Submit")).scrollTo();
+       $("#submit").click();
+
+        $("#output").shouldHave(text("John"), text("test@email.com"), text("Nowhere"), text("Here"));
+
+    }
+
+}
